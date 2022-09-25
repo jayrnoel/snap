4aee469cdec5e7918866a58c96b7bf77430733c2
jayrnoel <8006157+jayrnoel@users.noreply.github.com>
2022-09-26 00:00:59 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const overlay = document.querySelector(".overlay");
+const menuBar = document.querySelector(".menu__bar");
+const close = document.querySelector(".nav__close");
+const navBar = document.querySelector(".nav__bar");
+const companyLink = document.querySelector(".company__link");
+const company = document.querySelector(".company");
+const featuresLink = document.querySelector(".features__link");
+const feature = document.querySelector(".features");
+
+menuBar.addEventListener("click", () => {
+  overlay.classList.add("open");
+  navBar.classList.add("open");
+});
+
+overlay.addEventListener("click", () => {
+  closeMenu();
+});
+
+close.addEventListener("click", () => {
+  closeMenu();
+});
+
+featuresLink.addEventListener("click", (e) => {
+  feature.classList.toggle("open");
+  let arrow = e.target.querySelector(".arrow");
+  arrow.classList.toggle("open");
+});
+
+companyLink.addEventListener("click", (e) => {
+  company.classList.toggle("open");
+  let arrow = e.target.querySelector(".arrow");
+  arrow.classList.toggle("open");
+});
+
+function closeMenu() {
+  overlay.classList.remove("open");
+  navBar.classList.remove("open");
+}
